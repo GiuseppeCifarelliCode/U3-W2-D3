b6348fd9d5fbc189ddcf212bcb78058a6c133d75
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-07-26 17:57:58 +0200

    Without log

--- posts/src/app/post.service.ts
+++ posts/src/app/post.service.ts
@@ -34,7 +34,7 @@ export class PostService implements OnInit {
       },
       body: JSON.stringify(post)
     }).then(res => res.json())
-    .then(data => console.log(data)
-    )
+     // .then(data => console.log(data)
+    // )
   }
 }
